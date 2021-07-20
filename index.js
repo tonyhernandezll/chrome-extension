440861d6321be3b3d9042025c79f1e7048c7af8d
Tony <tonyhll1972@gmail.com>
2021-07-19 21:38:53 -0500

    added event listener for onclick event everytime someone clicks on the button

---
+++ index.js
@@ -0,0 +1,11 @@
+// function saveLead() {
+//   console.log("button clicked from onclick action");
+// }
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+
+inputBtn.addEventListener("click", function() {
+  console.log("Button Clicked from addEventListener");
+});
